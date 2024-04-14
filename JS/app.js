7c0d0554837ff3f7399402043f8a9780e382b53b
RodrigoDelgado005 <162932332+RodrigoDelgado005@users.noreply.github.com>
2024-04-14 17:53:18 -0300

    first commit

---
+++ JS/app.js
@@ -0,0 +1,7 @@
+document.getElementById('enlace').addEventListener('click', (e) => {
+    e.preventDefault()
+    
+    let url = e.target.getAttribute('href')
+    let pal = document.getElementById('palabra').value
+    window.open(url + '/search?q=' + pal)
+})
